b1c68197d971f034d46dce2d0a8e40ada8e580ea
rnintai <ho46825@gmail.com>
2023-09-12 16:36:20 +0900

    Feat: Speaker overlay 예외 처리

--- client/src/components/SpeakerImage/SpeakerImage.tsx
+++ client/src/components/SpeakerImage/SpeakerImage.tsx
@@ -8,12 +8,16 @@ interface speakerImageProps extends React.ComponentPropsWithoutRef<"div"> {
   alt: string;
 }
 
+const blacklistURL = [
+  "upload/china/speakers/5048c8e0-f2d6-428f-b4e2-79bf57317ccd_1694496145881.jpg",
+];
+
 const SpeakerImage = (props: speakerImageProps) => {
   const { src, alt, ...rest } = props;
   return (
     <SpeakerImageContainer {...rest}>
       <img className="speaker-image" src={`${S3_URL}/${src}`} alt={alt} />
-      <div className="overlay" />
+      {!blacklistURL.includes(src) && <div className="overlay" />}
     </SpeakerImageContainer>
   );
 };
